ca3723becc740171a5f49abd5b94b1f344f59561
Mo Chahadeh <applednd2002@gmail.com>
2023-05-08 18:35:54 +0300

    multi-processing

--- multi-processing/main.c
+++ multi-processing/main.c
@@ -1,15 +1,14 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <pthread.h>
 
 #define NO_OF_PRODUCTS 6
 #define NO_OF_CUSTOMERS 10
 #define NO_OF_THREADS 5
-
-
-
 
 struct Product {
   int product_id;
@@ -28,7 +27,8 @@ struct Customer {
 
 typedef struct {
   pthread_mutex_t product_locks[NO_OF_PRODUCTS];
-  Product products[NO_OF_PRODUCTS];
+  struct Product products[NO_OF_PRODUCTS];
+  struct Customer customers[NO_OF_CUSTOMERS];
 } shared_data_t;
 
 
@@ -46,7 +46,7 @@ typedef struct {
   int size;
 } ArrayArgs;
 
-void* order_product(void* arg) {
+void* order_product(shared_data_t *shared_data, void* arg) {
 
   ThreadArgs* args = (ThreadArgs*) arg;
   ThreadArgs myargs = *args;
@@ -55,43 +55,45 @@ void* order_product(void* arg) {
   int product_id = myargs.product_id +1;
   int direct = myargs.direct;
 
-  pthread_mutex_lock(&product_locks[product_id-1]);
+  pthread_mutex_lock(&(shared_data->product_locks[product_id-1]));
 
-  customers[customer_id-1].ordered_items[customers[customer_id-1].ordered_items_size][0] = (int) product_id;
-  customers[customer_id-1].ordered_items[customers[customer_id-1].ordered_items_size][1] = (int) ordered_quantity;
-  customers[customer_id-1].ordered_items_size++;
+  shared_data->customers[customer_id-1].ordered_items[shared_data->customers[customer_id-1].ordered_items_size][0] = (int) product_id;
+  shared_data->customers[customer_id-1].ordered_items[shared_data->customers[customer_id-1].ordered_items_size][1] = (int) ordered_quantity;
+  shared_data->customers[customer_id-1].ordered_items_size++;
 
 
-  printf("Customer %d: -Balance: %d, -Quantity Ordered: %d\nProduct %d: -Price: %d, -Quantity in stock: %d\n", customer_id, customers[customer_id-1].balance, ordered_quantity, product_id, products[product_id-1].price, products[product_id-1].quantity_in_stock);
+  printf("Customer %d: -Balance: %d, -Quantity Ordered: %d\nProduct %d: -Price: %d, -Quantity in stock: %d\n", customer_id, shared_data->customers[customer_id-1].balance, ordered_quantity, product_id, shared_data->products[product_id-1].price, shared_data->products[product_id-1].quantity_in_stock);
 
-  if(products[product_id-1].quantity_in_stock < ordered_quantity) {
+  if(shared_data->products[product_id-1].quantity_in_stock < ordered_quantity) {
     printf("Customer %d RESULT: Customer %d was unable to purchase Product %d because there isn't enough stock\n",customer_id, customer_id, product_id);
   }
 
-  else if(customers[customer_id-1].balance < products[product_id-1].price*ordered_quantity) {
+  else if(shared_data->customers[customer_id-1].balance < shared_data->products[product_id-1].price*ordered_quantity) {
     printf("Customer %d RESULT: Customer %d was unable to purchase Product %d because of insufficient balance\n",customer_id, customer_id, product_id);
   }
 
   else {
 
 
-  printf("Customer %d RESULT: Customer %d purchased %d of Product %d. Previous Quantity: %d. New Quantity: %d \n", customer_id ,customer_id,ordered_quantity, product_id, products[product_id-1].quantity_in_stock, products[product_id-1].quantity_in_stock-ordered_quantity);
-  products[product_id-1].quantity_in_stock -= ordered_quantity;
+  printf("Customer %d RESULT: Customer %d purchased %d of Product %d. Previous Quantity: %d. New Quantity: %d \n", customer_id ,customer_id,ordered_quantity, product_id, shared_data->products[product_id-1].quantity_in_stock, shared_data->products[product_id-1].quantity_in_stock-ordered_quantity);
+  shared_data->products[product_id-1].quantity_in_stock -= ordered_quantity;
 
-  customers[customer_id-1].purchased_items[customers[customer_id-1].purchased_items_size][0] = product_id;
-  customers[customer_id-1].purchased_items[customers[customer_id-1].purchased_items_size][1] = ordered_quantity;
-  customers[customer_id-1].purchased_items_size++;
+  shared_data->customers[customer_id-1].purchased_items[shared_data->customers[customer_id-1].purchased_items_size][0] = product_id;
+  shared_data->customers[customer_id-1].purchased_items[shared_data->customers[customer_id-1].purchased_items_size][1] = ordered_quantity;
+  shared_data->customers[customer_id-1].purchased_items_size++;
 
-  sleep(1);
+  sleep(3);
 
   }
-  pthread_mutex_unlock(&product_locks[product_id-1]);
+  pthread_mutex_unlock(&(shared_data->product_locks[product_id-1]));
 
   if(direct) free(arg);
   return NULL;
 }
 
-void* order_products(void* arg) {
+void* order_products(int shmid, void *arg) {
+
+  shared_data_t *shared_data = (shared_data_t *) shmat(shmid, NULL, 0);
 
   ArrayArgs* args = (ArrayArgs*) arg;
   ArrayArgs myArgs = *args;
@@ -102,27 +104,12 @@ void* order_products(void* arg) {
 
   printf("Customer %d ordered %d orders\n", customer_id, size);
 
-  pthread_t my_subthreads[size];
-
   for(int i = 0; i<size; i++) {
 
-    int err = pthread_create(&my_subthreads[i],NULL,order_product,(void*) &orders[i]);
-
-    if(err) {
-      perror("subthreads create error");
-      exit(1);
-    }
+    order_product(shared_data, (void*) &orders[i]);
 
   }
 
-  for(int i = 0; i<size; i++) {
-    int err = pthread_join(my_subthreads[i], NULL);
-
-    if(err) {
-      perror("subthreads join error");
-      exit(1);
-    }
-  }
 
   return NULL;
 
@@ -130,121 +117,99 @@ void* order_products(void* arg) {
 
 int main(int argc, char const *argv[]) {
 
-  srand(time(NULL));
-  for (int i = 0; i < NO_OF_PRODUCTS; i++) {
+  srand(time(0));
+
+  int shmid;
+  shared_data_t *shared_data;
 
 
-    products[i].product_id = i+1;
-    products[i].price = (rand() %200) + 1;
-    products[i].quantity_in_stock = (rand() % 10) + 1;
-
-    printf("Product Id: %d, Product Price: %d, Product Quantity: %d \n", products[i].product_id, products[i].price, products[i].quantity_in_stock);
-
-    pthread_mutex_init(&product_locks[i], NULL);
-
+  shmid = shmget(IPC_PRIVATE, sizeof(shared_data_t), IPC_CREAT | 0666);
+  if(shmid == -1){
+    perror("shmget error");
+    exit(1);
   }
 
-  for(int i = 0; i< NO_OF_CUSTOMERS; i++) {
 
-    customers[i].customer_id = i+1;
-    customers[i].balance = (rand() %500) +1;
+  shared_data = (shared_data_t *) shmat(shmid, NULL, 0);
 
-    printf("Customer %d, Balance: %d\n", customers[i].customer_id, customers[i].balance);
-
+  if(shared_data == (shared_data_t *) -1){
+    perror("shmat error");
+    exit(1);
   }
 
-  pthread_t my_threads[NO_OF_CUSTOMERS];
-
-  for(int i = 0; i< NO_OF_CUSTOMERS; i++) {
-
-    int no_of_orders = rand()%5 + 1;
-
-    ThreadArgs* orders = (ThreadArgs*) malloc(no_of_orders*sizeof(ThreadArgs));
-
-    int customer = i;
-
-    for(int i = 0; i<no_of_orders; i++) {
-
-      orders[i].customer_id = customer;
-      orders[i].product_id = (rand() % NO_OF_PRODUCTS);
-      orders[i].product_quantity = (rand() % 4 + 1);
-      orders[i].direct = 0;
-
-    }
-
-    ArrayArgs* array_args = (ArrayArgs*) malloc(sizeof(ArrayArgs));
-
-    array_args->customer = customer;
-    array_args->orders = orders;
-    array_args->size =  no_of_orders;
-
-    int err1 = pthread_create(&my_threads[i], NULL, order_products, (void *) array_args);
-
-    if(err1) {
-      perror("thread create error\n");
-      exit(1);
-    }
 
 
+  pthread_mutexattr_t mutex_attr;
+  pthread_mutexattr_init(&mutex_attr);
+  pthread_mutexattr_setpshared(&mutex_attr, PTHREAD_PROCESS_SHARED);
+
+
+  for(int i = 0; i<NO_OF_PRODUCTS; i++) {
+
+    printf("Product %d\n", i+1);
+    shared_data->products[i].product_id = i+1;
+    shared_data->products[i].price = (rand() % 200) + 1;
+    shared_data->products[i].quantity_in_stock = (rand() %10) + 1;
+
+    pthread_mutex_init(&(shared_data->product_locks[i]), &mutex_attr);
   }
 
+  for(int i = 0; i<NO_OF_CUSTOMERS; i++) {
+    printf("Customer %d\n", i+1);
+
+    shared_data->customers[i].customer_id = i+1;
+    shared_data->customers[i].balance = (rand() % 200) + 1;
+
+  }
 
   for(int i = 0; i<NO_OF_CUSTOMERS; i++) {
 
-    int rc = pthread_join(my_threads[i], NULL);
+    int pid = fork();
 
-    if(rc != 0) {
-      perror("Pthread join error");
+    if(pid < 0) {
+      perror(" fork error ");
       exit(1);
     }
-  }
 
+    else if(pid == 0){ // Child Process
 
-  printf("\nPress any key to show summaries of customers...\n");
-  getchar();
+      printf("child process\n");
+      int no_of_orders = rand()%5 + 1;
 
-  printf("\nSUMMARIES:\n");
-  sleep(1);
+      ThreadArgs* orders = (ThreadArgs*) malloc(no_of_orders*sizeof(ThreadArgs));
 
-  for (int i = 0; i<NO_OF_CUSTOMERS; i++) {
+      int customer = i;
 
-    printf("\n----------------\n\nCustomer %d Summary:\n", i+1);
-    sleep(1);
+      for(int j = 0; j<no_of_orders; i++) {
 
-    printf("\nOrdered Products:\n\n");
-    printf("%-15s %-15s \n", "Product ID", "Quantity");
+        orders[i].customer_id = customer;
+        orders[i].product_id = (rand() % NO_OF_PRODUCTS);
+        orders[i].product_quantity = (rand() % 4 + 1);
+        orders[i].direct = 0;
 
-    for(int j = 0; j<customers[i].ordered_items_size;j++){
+      }
 
-      int product_id = customers[i].ordered_items[j][0];
-      int quantity =   customers[i].ordered_items[j][1];
+      ArrayArgs* array_args = (ArrayArgs*) malloc(sizeof(ArrayArgs));
 
-      printf("%-15d %-15d \n",  product_id, quantity);
+      array_args->customer = customer;
+      array_args->orders = orders;
+      array_args->size =  no_of_orders;
 
-    }
+      order_products(shmid, array_args);
 
-    sleep(2);
+      break;
 
-    printf("\nPurchased Products:\n\n");
-    printf("%-15s %-15s \n", "Product ID", "Quantity");
-
-    for(int j = 0; j<customers[i].purchased_items_size; j++) {
-
-      int product_id = customers[i].purchased_items[j][0];
-      int quantity =   customers[i].purchased_items[j][1];
-
-      printf("%-15d %-15d \n",  product_id,  quantity);
-
-    }
-
-    sleep(4);
+    } else continue; // parent process
 
   }
 
-  printf("\n\n PROGRAM END \n\n");
 
 
-  pthread_exit(NULL);
+
+  if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+    perror("shmctl error");
+    return 1;
+  }
 
   return 0;
 }
